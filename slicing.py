53d2055b643f367c42ffa66d70d57e3f2495d00b
meerajasmin-7 <meerajasmin6220@gmail.com>
2026-06-28 15:59:19 +0530

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,15 @@
+# Atleast 10 different output generate in SLICING
+
+l=[10,20,30,40,50]
+print(l[:])
+print(l[4:])
+print(l[1:4:1])
+print(l[1:5])
+print(l[1:])
+print(l[:5])
+print(l[-5:-1:1])
+print(l[-6:-2])
+print(l[:-1])
+print(l[-1:])
+print(l[:-2])
+print(l[:-5])
