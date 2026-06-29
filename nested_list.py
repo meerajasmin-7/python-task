21689fb494c3e7ad4f6d2e3f055d8626a73ac1fa
meerajasmin-7 <meerajasmin6220@gmail.com>
2026-06-30 00:42:10 +0530

    Add files via upload

---
+++ nested_list.py
@@ -0,0 +1,30 @@
+# list
+
+list = [10, 20, 30, "Python"]
+print(list[1])
+list.append("java")
+print(list)
+
+# nested list
+
+matrix = [[1, 2], [3, 4], [5, 6]]
+print(matrix[0])      # [1, 2]
+print(matrix[1][1])   # 4
+
+
+# tuple
+
+tuple = (10, 20, 30)
+print(tuple[1])
+
+# set
+
+numbers = {1, 2, 2, 3, 4}
+print(numbers)
+
+# Dictionary
+
+student = {"name": "jasmin", "age": 20, "courese": "python"}
+print(student["name"])   # jasmin
+student["age"] = 20
+print(student,student["courese"])
